c68350b9b10214af1213d736ff76f75033a8bebf
Alvaro Romano <alvaro5390@gmail.com>
2023-11-05 21:03:04 +0100

    dark-theme terminado

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log("working");
+document.addEventListener("DOMContentLoaded", () => {
+    let icon = document.querySelector(".fa-solid");
+    let header = document.querySelector("h1");
+    icon.addEventListener("click", () => {
+      document.body.classList.toggle("light");
+      document.body.classList.toggle("dark");
+      document.body.classList.toggle("tour");
+      icon.classList.toggle("fa-sun");
+      icon.classList.toggle("fa-moon");
+      icon.classList.toggle("spin");
+      header.classList.toggle("smack");
+      if (header.textContent == "Good Morning!") {
+        header.textContent = "Good Night!";
+      } else {
+        header.textContent = "Good Morning!";
+      }
+    });
+  });
+  
